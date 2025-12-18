97b9102b627dbfab248ae3cf7124b33f90a992fc
torosyan-99 <toorosyan99@gmail.com>
2025-12-18 18:28:42 +0400

    add

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const paymentsSwiper = document.querySelector('.payments__swiper')
 
-if(paymentsSwiper) {
+if (paymentsSwiper) {
   let swiper;
   let isDesktop = window.innerWidth >= 768;
 
   function initSwiper() {
     if (swiper) swiper.destroy(true, true);
 
-    swiper = new Swiper('.payments__swiper', {
+    swiper = new Swiper(paymentsSwiper, {
       spaceBetween: 16,
       loop: true,
       slidesPerView: 1.57,
@@ -92,3 +92,28 @@ if (modal) {
     e.stopPropagation();
   })
 }
+
+const controllers = document.querySelectorAll('.controllers');
+
+[...controllers].forEach((controller) => {
+  let activeControllerButton = controller.querySelector('.controllers__button--active')
+  const items = {};
+
+  [...controller.parentElement.querySelectorAll('.controllers__item')].forEach(item => {
+    console.log(item, 'item')
+    items[item.dataset.index] = item
+  })
+  console.log(items)
+  controller.addEventListener('click', (e) => {
+    const button = e.target.closest('.controllers__button');
+
+    if (button === activeControllerButton) return;
+
+    activeControllerButton.classList.remove('controllers__button--active');
+    items[activeControllerButton.dataset.index].classList.remove('controllers__item--active')
+    button.classList.add('controllers__button--active')
+    items[button.dataset.index].classList.add('controllers__item--active')
+
+    activeControllerButton = button;
+  })
+});
